a663ea92c51f5ffcaa2045ea84f7b6963cf6faab
unknown <wael.mrabet@esprit.tn>
2021-09-30 00:53:07 +0200

    test changes from aymen pc

--- Data/Repositories/IBusninessTypeRepository.cs
+++ Data/Repositories/IBusninessTypeRepository.cs
@@ -10,5 +10,7 @@ namespace Data.Repositories
 
         ICollection<BusinessType> GetActivatedBusinessTypes();
         ICollection<BusinessType> GetBusinessInListMapCodes(List<string> businessTypesIds);
+
+// test
     }
 }
